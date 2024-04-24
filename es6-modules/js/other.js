3e183b4cd49323e8f58f9b23844e10a3eb46ac43
Tim Carlson <timca@arenasports.net>
2024-04-24 12:22:26 -0700

    starter branch for es-6 modules

---
+++ es6-modules/js/other.js
@@ -0,0 +1,27 @@
+'use strict';
+
+console.log("I'm the other.js file!");
+
+// //Slide 16a
+// console.log("Animal Array: ", animalArray)
+
+// // //Slide 18
+// const otherMessage = "I'm other!";
+
+// //Slide 18a
+// export const otherMessage = "I'm other!";
+
+// //Slide 18b
+// export function otherFunc() {
+//     console.log("Text from 'otherFunc()' ");
+// }
+
+// //Slide 20 default example
+// export const otherMessage = "I'm other!";
+
+// export default function otherFunc() {
+//     console.log("Text from 'otherFunc()' ");
+// }
+
+
+ 
